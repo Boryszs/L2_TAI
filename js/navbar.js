1926c55ce5defb9dbb6474198ad3b0ea6cf3ea6e
Boryszs <12damie@gmail.com>
2021-03-19 18:07:53 +0100

    implement navbar script with navigation.

---
+++ js/navbar.js
@@ -0,0 +1,4 @@
+let nav = '<nav><a href="index.html">Strona główna</a> ' +
+    '<a href="contact.html">Kontakt</a></nav>'
+let navbarElement = document.querySelector(".navbar");
+navbarElement.innerHTML = nav
